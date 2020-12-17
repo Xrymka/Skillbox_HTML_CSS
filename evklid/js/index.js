a11dcfb0113a4d6bf63da493d52b54bfe2cf106b
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2020-12-17 15:45:42 +0300

    WIP

--- evklid/js/index.js
+++ evklid/js/index.js
@@ -1,5 +1,17 @@
+/* Burger menu */
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#burger').addEventListener('click', function() {
     document.querySelector('#menu').classList.toggle('active')
   })
+})
+
+/* Swiper */
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 })
