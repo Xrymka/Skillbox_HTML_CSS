0992064e621d9019a02e5ab9a8d912c4ed809be5
Kate Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2021-05-20 23:34:57 +0500

    gulp wip

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,11 +3,28 @@ const concat = require('gulp-concat');
 const htmlMin = require('gulp-htmlmin');
 const autoprefixes = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const svgSprite = require('gulp-svg-sprite');
+const image = require('gulp-image');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify-es').default;
+const notify = require('gulp-notify');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del')
 const browserSync = require('browser-sync').create();
+
+const clean = () => {
+  return del(['dist'])
+}
+
+const resources = () => {
+  return src('src/resources/**')
+    .pipe(dest('dist'))
+}
 
 
 const styles = () => {
   return src('src/styles/**/*.css')
+    .pipe(sourcemaps.init())
     .pipe(concat('main.css'))
     .pipe(autoprefixes({
       cascade: false
@@ -15,6 +32,7 @@ const styles = () => {
     .pipe(cleanCSS({
       level: 2
     }))
+    .pipe(sourcemaps.write())
     .pipe(dest('dist'))
     .pipe(browserSync.stream())
 };
@@ -28,17 +46,63 @@ const htmlMinify = () => {
     .pipe(browserSync.stream())
 };
 
+const svgSprites = () => {
+  return src('src/images/svg/**/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg'
+        }
+      }
+    }))
+    .pipe(dest('dist/images'))
+}
+
+const scripts = () => {
+  return src([
+    'src/js/components/**/*.js',
+    'src/js/main.js'
+  ])
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(concat('app.js'))
+  .pipe(uglify({
+    toplevel: true
+  }).on('error', notify.onError()))
+  .pipe(sourcemaps.write())
+  .pipe(dest('dist'))
+  .pipe(browserSync.stream())
+}
+
 const watchFiles = () => {
   browserSync.init({
+    open: "external",
     server: {
       baseDir: 'dist'
     }
   })
 }
 
+const images = () => {
+  return src([
+    'src/images/**/*.jpg',
+    'src/images/**/*.jpeg',
+    'src/images/**/*.png',
+    'src/images/*.svg'
+  ])
+  .pipe(image())
+  .pipe(dest('dist/images'))
+}
+
 watch('src/**/*.html', htmlMinify)
 watch('src/**/*.css', styles)
+watch('src/images/svg/**/*/svg', svgSprites)
+watch('src/js/**/*.js', scripts)
+watch('src/resources/**', resources)
 
 exports.styles = styles;
+exports.scripts = scripts;
 exports.htmlMinify = htmlMinify;
-exports.default = series(htmlMinify, styles, watchFiles)
+exports.default = series(clean, resources, htmlMinify, scripts, styles, images, svgSprites, watchFiles)
